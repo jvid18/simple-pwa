2031c5c56f4a0ff67c23efee44128ce422603006
jvid <judara2000@gmail.com>
2022-02-20 13:10:50 -0500

    Fix: Service worker route

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ const DYNAMIC_CACHE = 'dynamic-v1'
 const INMUTABLE_CACHE = 'inmutable-v1'
 
 const APP_SHELL = [
-  '/',
+  // '/',
   'index.html',
   'css/style.css',
   'img/favicon.ico',
@@ -22,33 +22,41 @@ const APP_SHELL_INMUTABLE = [
   'https://fonts.googleapis.com/css?family=Lato:400,300',
   'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
   'css/animate.css',
-  'js/libs/jquery.js'
+  'js/libs/jquery.js',
 ]
 
-self.addEventListener('install', e => {
+self.addEventListener('install', (e) => {
+  const cacheStatic = caches
+    .open(STATIC_CACHE)
+    .then((cache) => cache.addAll(APP_SHELL))
+  const cacheInmutable = caches
+    .open(INMUTABLE_CACHE)
+    .then((cache) => cache.addAll(APP_SHELL_INMUTABLE))
 
-  const cacheStatic = caches.open(STATIC_CACHE).then(cache => cache.addAll(APP_SHELL))
-  const cacheInmutable = caches.open(INMUTABLE_CACHE).then(cache => cache.addAll(APP_SHELL_INMUTABLE))
-
-  const cachePromise = Promise.all([ cacheStatic, cacheInmutable ])
+  const cachePromise = Promise.all([cacheStatic, cacheInmutable])
 
   e.waitUntil(cachePromise)
 })
 
-self.addEventListener('activate', e => {
-  const cleanPromises = Promise.all([ cleanStaticCache(), cleanInmutableCache(), cleanDynamicCache() ])
+self.addEventListener('activate', (e) => {
+  const cleanPromises = Promise.all([
+    cleanStaticCache(),
+    cleanInmutableCache(),
+    cleanDynamicCache(),
+  ])
 
   e.waitUntil(cleanPromises)
 })
 
-self.addEventListener('fetch', e => {
-  const res = caches.match(e.request).then(res => {
-    if(res) return res
+self.addEventListener('fetch', (e) => {
+  const res = caches.match(e.request).then((res) => {
+    if (res) return res
 
-    return fetch(e.request).then(newRes => {
+    return fetch(e.request).then((newRes) => {
       return updateDynamicCache(DYNAMIC_CACHE, e.request, newRes)
     })
   })
 
   return e.respondWith(res)
 })
+
